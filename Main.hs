7b6169cfee4b97c3554f6c89b217069f4bf862ab
Adriel Bergmann <adrielb@users.noreply.github.com>
2014-07-02 11:49:59 -0400

    testing readline and list comprehension

--- Main.hs
+++ Main.hs
@@ -1,9 +1,23 @@
 import Data.Algorithm.Munkres
 import Data.Array.Unboxed 
+import System.Console.Readline
 
+
+readInput :: Int -> IO()
+readInput id = do
+  maybeLine <- readline ("User " ++ show(id) ++ ": ")
+  case maybeLine of
+    Nothing -> return ()
+    Just line -> putStrLn $ read (show line)
+
+getCost :: Int -> Int -> Int
+getCost user partner = user * partner
 
 main :: IO ()
 main = do
+  readInput 3
+  let x = [((user, partner), getCost user partner)| user <- [1..3], partner <- [1..3]]
+  print x
   let myarray = array ((1,1),(4,4)) [((4,4),1)] :: UArray (Int, Int) Int
   let result = hungarianMethodInt myarray
   print myarray
