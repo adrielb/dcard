265a985c78090fdf26a3a2cec1cc3d3c028a29dc
Adriel Bergmann <adrielb@users.noreply.github.com>
2014-07-17 12:17:11 -0400

    split main into seperate functions (input/uarray)

--- Main.hs
+++ Main.hs
@@ -14,11 +14,19 @@ getCost :: Int -> Int -> Int
 getCost user partner = user * partner
 
 main :: IO ()
-main = do
+main = testUArray
+
+testInput :: IO()
+testInput = do
   readInput 3
   let x = [((user, partner), getCost user partner)| user <- [1..3], partner <- [1..3]]
   print x
+
+testUArray :: IO()
+testUArray = do
   let myarray = array ((1,1),(4,4)) [((4,4),1)] :: UArray (Int, Int) Int
   let result = hungarianMethodInt myarray
   print myarray
   print result
+
+
